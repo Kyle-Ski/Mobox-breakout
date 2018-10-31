63a6eeb9f4a966707cfbbe1807005eb04b146c80
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-31 14:40:25 -0600

    made button alternate emojis

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ import Box2 from './box2'
         <Box1 store={this.props.store} />
         <Box2 store={this.props.store} />
         </div>
-        <button onClick={this.increment}> 🎃 bump 🎃 </button>
+        <button onClick={this.increment}> bump {this.props.store.count.length % 4 === 2 ? '☠️':'🎃'} </button>
       </div>
     );
   }
